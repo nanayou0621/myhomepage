1a33ac9d1507b2ecbaf8a8070369eda34dd01b36
nanayou0621 <132759432+nanayou0621@users.noreply.github.com>
2023-07-21 09:59:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,5 +23,42 @@ pagetop.addEventListener("click",()=>{
       behavior:"smooth"
   });
 });
-  
+
+const sub = document.querySelector('.sub-title');
+
+const keyframes = {
+  opacity:[0,1],
+  translate:['0 50px',0],
+};
+const options = {
+  duration:2000,
+  easing:'ease',
+};
+sub.animate(keyframes,options);
+
+
+// ギャラリー カルーセル
+
+const ul = document.querySelector('.gallery-ul');
+const slides = ul.children;
+const dots =[];
+let currentIndex = 0;
+
+function moveSlides() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+}
+function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      button.addEventListener('click' ,() =>{
+        currentIndex = i;
+        moveSlides();
+      });
+      dots.push(button);
+      document.querySelector('.nav-button').appendChild(button);
+    }
+    dots[0].classList.add('current');
+  }
+  setupDots();
 }
